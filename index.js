d31cb6fd8317a0df92128103eb376600d552d149
Francis Lewis <francis.lewis@me.com>
2019-04-11 16:09:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function happyHolidays() {
 
 // write your code below!
 function happyHolidaysTo(name) {
-  return `Happy holidays, {name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, {name}!`
+  return `Happy ${holiday}, ${name}!`
 }
